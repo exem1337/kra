d0787ef5e8d7771d49dd06e668c43f34d793c09f
Tripper567 <mikerin_da@office.osu.ru>
2022-12-13 01:04:09 +0500

    2

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.t1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.t2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.t3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.t4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -41,15 +45,31 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.t1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.t2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.t3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.t4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -76,6 +96,26 @@
             this.dataGridView1.Size = new System.Drawing.Size(453, 150);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // t1
+            // 
+            this.t1.HeaderText = "План выпуска алюм. радиаторов";
+            this.t1.Name = "t1";
+            // 
+            // t2
+            // 
+            this.t2.HeaderText = "Факт выпуска алюм. радиаторов";
+            this.t2.Name = "t2";
+            // 
+            // t3
+            // 
+            this.t3.HeaderText = "План выпуска медных радиаторов";
+            this.t3.Name = "t3";
+            // 
+            // t4
+            // 
+            this.t4.HeaderText = "Факт выпуска медных радиаторов";
+            this.t4.Name = "t4";
             // 
             // label1
             // 
@@ -184,6 +224,7 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.groupBox1);
             this.tabPage2.Location = new System.Drawing.Point(4, 24);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
@@ -191,15 +232,6 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Подбор оптимальной альтернативы";
             this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // tabPage3
-            // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 24);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(877, 382);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Справка";
-            this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // tabPage4
             // 
@@ -210,25 +242,138 @@
             this.tabPage4.Text = "Статистический отчет";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
-            // t1
+            // tabPage3
             // 
-            this.t1.HeaderText = "План выпуска алюм. радиаторов";
-            this.t1.Name = "t1";
+            this.tabPage3.Location = new System.Drawing.Point(4, 24);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(877, 382);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Справка";
+            this.tabPage3.UseVisualStyleBackColor = true;
             // 
-            // t2
+            // groupBox1
             // 
-            this.t2.HeaderText = "Факт выпуска алюм. радиаторов";
-            this.t2.Name = "t2";
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.dataGridView2);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(868, 259);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Метод максимакса";
             // 
-            // t3
+            // label9
             // 
-            this.t3.HeaderText = "План выпуска медных радиаторов";
-            this.t3.Name = "t3";
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 34);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(168, 15);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Расчет критерия максимакса";
             // 
-            // t4
+            // pictureBox1
             // 
-            this.t4.HeaderText = "Факт выпуска медных радиаторов";
-            this.t4.Name = "t4";
+            this.pictureBox1.Location = new System.Drawing.Point(196, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(220, 42);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(435, 34);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(164, 15);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "Правило принятия решения";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(625, 22);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(234, 42);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView2.Location = new System.Drawing.Point(6, 75);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 25;
+            this.dataGridView2.Size = new System.Drawing.Size(689, 126);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Тип";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Стойкость прокладок (%)";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Устойчивость к вибрациям (Гц)";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Стоимость (тыс. рублей)";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Сроки службы (года)";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Критерий";
+            this.Column6.Name = "Column6";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(718, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 63);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Найти оптимальную альтернативу";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(681, 213);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(165, 15);
+            this.label11.TabIndex = 6;
+            this.label11.Text = "Оптимальная альтернатива :";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(681, 241);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(44, 15);
+            this.label12.TabIndex = 7;
+            this.label12.Text = "label12";
             // 
             // Form1
             // 
@@ -242,6 +387,12 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -267,5 +418,20 @@
         private DataGridViewTextBoxColumn t2;
         private DataGridViewTextBoxColumn t3;
         private DataGridViewTextBoxColumn t4;
+        private GroupBox groupBox1;
+        private PictureBox pictureBox1;
+        private Label label9;
+        private DataGridView dataGridView2;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
+        private DataGridViewTextBoxColumn Column6;
+        private PictureBox pictureBox2;
+        private Label label10;
+        private Label label12;
+        private Label label11;
+        private Button button2;
     }
 }
