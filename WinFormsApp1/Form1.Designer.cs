9eb0a2183eda2e1a322e77cbd98568f75690f645
Tripper567 <mikerin_da@office.osu.ru>
2022-12-13 16:19:39 +0500

    www

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -44,6 +44,7 @@
             this.label8 = new System.Windows.Forms.Label();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label23 = new System.Windows.Forms.Label();
             this.label22 = new System.Windows.Forms.Label();
             this.label21 = new System.Windows.Forms.Label();
             this.label20 = new System.Windows.Forms.Label();
@@ -72,7 +73,6 @@
             this.label9 = new System.Windows.Forms.Label();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.label23 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -85,7 +85,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(444, 407);
+            this.button1.Location = new System.Drawing.Point(296, 581);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(264, 44);
             this.button1.TabIndex = 0;
@@ -101,15 +101,16 @@
             this.t2,
             this.t3,
             this.t4});
-            this.dataGridView1.Location = new System.Drawing.Point(19, 6);
+            this.dataGridView1.Location = new System.Drawing.Point(96, 403);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(453, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(677, 150);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // t1
             // 
+            this.t1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.t1.HeaderText = "План выпуска алюм. радиаторов";
             this.t1.Name = "t1";
             // 
@@ -131,7 +132,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(498, 57);
+            this.label1.Location = new System.Drawing.Point(30, 49);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(89, 15);
             this.label1.TabIndex = 2;
@@ -140,7 +141,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(498, 87);
+            this.label2.Location = new System.Drawing.Point(30, 79);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(167, 15);
             this.label2.TabIndex = 3;
@@ -149,7 +150,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(498, 114);
+            this.label3.Location = new System.Drawing.Point(30, 106);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(83, 15);
             this.label3.TabIndex = 4;
@@ -158,7 +159,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(498, 142);
+            this.label4.Location = new System.Drawing.Point(30, 134);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(83, 15);
             this.label4.TabIndex = 5;
@@ -167,7 +168,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(498, 173);
+            this.label5.Location = new System.Drawing.Point(30, 165);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(56, 15);
             this.label5.TabIndex = 6;
@@ -176,7 +177,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(498, 204);
+            this.label6.Location = new System.Drawing.Point(30, 196);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(63, 15);
             this.label6.TabIndex = 7;
@@ -185,7 +186,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(498, 234);
+            this.label7.Location = new System.Drawing.Point(30, 226);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(183, 15);
             this.label7.TabIndex = 8;
@@ -195,11 +196,11 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label8.Location = new System.Drawing.Point(19, 260);
+            this.label8.Location = new System.Drawing.Point(30, 275);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(188, 21);
+            this.label8.Size = new System.Drawing.Size(364, 21);
             this.label8.TabIndex = 9;
-            this.label8.Text = "Уравнение регрессии Al :";
+            this.label8.Text = "Уравнение регрессии алюминиевых радиаторов :";
             // 
             // tabControl1
             // 
@@ -210,7 +211,7 @@
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1240, 495);
+            this.tabControl1.Size = new System.Drawing.Size(925, 685);
             this.tabControl1.TabIndex = 10;
             // 
             // tabPage1
@@ -239,16 +240,26 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 24);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1232, 467);
+            this.tabPage1.Size = new System.Drawing.Size(917, 657);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Прогнозирование";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label23.Location = new System.Drawing.Point(30, 361);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(428, 21);
+            this.label23.TabIndex = 20;
+            this.label23.Text = "В следующем месяце требуется увеличить производство :";
             // 
             // label22
             // 
             this.label22.AutoSize = true;
             this.label22.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label22.Location = new System.Drawing.Point(866, 20);
+            this.label22.Location = new System.Drawing.Point(422, 12);
             this.label22.Name = "label22";
             this.label22.Size = new System.Drawing.Size(188, 25);
             this.label22.TabIndex = 19;
@@ -258,26 +269,26 @@
             // 
             this.label21.AutoSize = true;
             this.label21.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label21.Location = new System.Drawing.Point(487, 20);
+            this.label21.Location = new System.Drawing.Point(19, 12);
             this.label21.Name = "label21";
-            this.label21.Size = new System.Drawing.Size(258, 25);
+            this.label21.Size = new System.Drawing.Size(247, 25);
             this.label21.TabIndex = 18;
-            this.label21.Text = "Аллюминиевые радиаторы";
+            this.label21.Text = "Алюминиевые радиаторы";
             // 
             // label20
             // 
             this.label20.AutoSize = true;
             this.label20.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label20.Location = new System.Drawing.Point(19, 317);
+            this.label20.Location = new System.Drawing.Point(30, 321);
             this.label20.Name = "label20";
-            this.label20.Size = new System.Drawing.Size(193, 21);
+            this.label20.Size = new System.Drawing.Size(318, 21);
             this.label20.TabIndex = 17;
-            this.label20.Text = "Уравнение регрессии Cu :";
+            this.label20.Text = "Уравнение регрессии медных радиаторов :";
             // 
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(895, 57);
+            this.label13.Location = new System.Drawing.Point(427, 49);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(89, 15);
             this.label13.TabIndex = 10;
@@ -286,7 +297,7 @@
             // label14
             // 
             this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(895, 234);
+            this.label14.Location = new System.Drawing.Point(427, 226);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(183, 15);
             this.label14.TabIndex = 16;
@@ -295,7 +306,7 @@
             // label15
             // 
             this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(895, 87);
+            this.label15.Location = new System.Drawing.Point(427, 79);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(167, 15);
             this.label15.TabIndex = 11;
@@ -304,7 +315,7 @@
             // label16
             // 
             this.label16.AutoSize = true;
-            this.label16.Location = new System.Drawing.Point(895, 204);
+            this.label16.Location = new System.Drawing.Point(427, 196);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(63, 15);
             this.label16.TabIndex = 15;
@@ -313,7 +324,7 @@
             // label17
             // 
             this.label17.AutoSize = true;
-            this.label17.Location = new System.Drawing.Point(895, 114);
+            this.label17.Location = new System.Drawing.Point(427, 106);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(83, 15);
             this.label17.TabIndex = 12;
@@ -322,7 +333,7 @@
             // label18
             // 
             this.label18.AutoSize = true;
-            this.label18.Location = new System.Drawing.Point(895, 173);
+            this.label18.Location = new System.Drawing.Point(427, 165);
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(56, 15);
             this.label18.TabIndex = 14;
@@ -331,7 +342,7 @@
             // label19
             // 
             this.label19.AutoSize = true;
-            this.label19.Location = new System.Drawing.Point(895, 142);
+            this.label19.Location = new System.Drawing.Point(427, 134);
             this.label19.Name = "label19";
             this.label19.Size = new System.Drawing.Size(83, 15);
             this.label19.TabIndex = 13;
@@ -343,7 +354,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 24);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1232, 417);
+            this.tabPage2.Size = new System.Drawing.Size(917, 657);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Подбор оптимальной альтернативы";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -360,7 +371,7 @@
             this.groupBox1.Controls.Add(this.label9);
             this.groupBox1.Location = new System.Drawing.Point(6, 6);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(868, 259);
+            this.groupBox1.Size = new System.Drawing.Size(900, 287);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Метод максимакса";
@@ -368,7 +379,7 @@
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(681, 241);
+            this.label12.Location = new System.Drawing.Point(23, 242);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(44, 15);
             this.label12.TabIndex = 7;
@@ -377,7 +388,7 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(681, 213);
+            this.label11.Location = new System.Drawing.Point(23, 214);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(165, 15);
             this.label11.TabIndex = 6;
@@ -477,7 +488,7 @@
             // 
             this.tabPage4.Location = new System.Drawing.Point(4, 24);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(1232, 417);
+            this.tabPage4.Size = new System.Drawing.Size(917, 657);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Статистический отчет";
             this.tabPage4.UseVisualStyleBackColor = true;
@@ -486,25 +497,16 @@
             // 
             this.tabPage3.Location = new System.Drawing.Point(4, 24);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(1232, 417);
+            this.tabPage3.Size = new System.Drawing.Size(917, 657);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Справка";
             this.tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // label23
-            // 
-            this.label23.AutoSize = true;
-            this.label23.Location = new System.Drawing.Point(19, 373);
-            this.label23.Name = "label23";
-            this.label23.Size = new System.Drawing.Size(329, 15);
-            this.label23.TabIndex = 20;
-            this.label23.Text = "В следующем месяце требуется увеличить производство :";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1264, 519);
+            this.ClientSize = new System.Drawing.Size(934, 701);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -539,10 +541,6 @@
         private TabPage tabPage2;
         private TabPage tabPage4;
         private TabPage tabPage3;
-        private DataGridViewTextBoxColumn t1;
-        private DataGridViewTextBoxColumn t2;
-        private DataGridViewTextBoxColumn t3;
-        private DataGridViewTextBoxColumn t4;
         private GroupBox groupBox1;
         private PictureBox pictureBox1;
         private Label label9;
@@ -569,5 +567,9 @@
         private Label label22;
         private Label label21;
         private Label label23;
+        private DataGridViewTextBoxColumn t1;
+        private DataGridViewTextBoxColumn t2;
+        private DataGridViewTextBoxColumn t3;
+        private DataGridViewTextBoxColumn t4;
     }
 }
